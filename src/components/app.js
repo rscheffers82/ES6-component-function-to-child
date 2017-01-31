8a3f93263bf8fe988324189b24742af8dee6997d
Roy Scheffers <r.scheffers@gmail.com>
2017-01-31 08:46:00 -0600

    Add app description and update styles

--- src/components/app.js
+++ src/components/app.js
@@ -16,9 +16,21 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="main">
-        <ToggleAnnotations onClick={this.handleClick}/>
-        <ViewAnnotations value={this.state.click}/>
+      <div>
+        <div className="project-details">
+          With three components in this app.
+          <ol>
+            <li>App.js manages state and passes a handle input function to ToggleAnnotations through props.</li>
+            <li>ToggleAnnotations.js receives input (onClick event) and passes calls the function from App.js</li>
+            <li>ViewAnnotations.js displays input through props.</li>
+          </ol>
+          <div className='note'>NOTE: <i>React Component class is used</i></div>
+        </div>
+
+        <div className="main">
+          <ToggleAnnotations onClick={this.handleClick}/>
+          <ViewAnnotations value={this.state.click}/>
+        </div>
       </div>
     );
   }
